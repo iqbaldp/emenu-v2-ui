4c4e5b1818da3d57cbb613d363fc71dcbe26414a
Iqbal DP <miqballdp@gmail.com>
2025-02-09 12:38:24 +0800

    add ecosystem config

---
+++ ecosystem.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  apps: [
+    {
+      name: 'emenu-v2',
+      script: 'node_modules/next/dist/bin/next',
+      args: 'start',
+      env: {
+        NODE_ENV: 'production',
+        PORT: 3200
+      }
+    }
+  ]
+}
